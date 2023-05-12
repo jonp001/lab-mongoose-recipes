be6c416bc5d3ac558b1c8777fd6bdfcfd98ed4fe
Jon Perez <jonperez01@outlook.com>
2023-05-11 22:06:25 -0400

    all iterations complete

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
+
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app'
+mongoose.set('strictQuery', true);
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+  
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -21,3 +22,54 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+Recipe.create({
+    "title": "Asian Glazed Chicken Thighs",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    "cuisine": "Asian",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    "duration": 40,
+    "creator": "Chef LePapu"
+  })
+
+.then ((firstRecipe) => {
+  console.log("The first recipe is called ", firstRecipe.title)
+})
+
+//iteration 3
+
+.then(() => {
+  Recipe.insertMany(data)
+})
+.then ((x) => {
+  console.log("The Array of Recipes added are: ", x)
+})
+
+//iteration 4: 
+.then(() => {
+  Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+})
+.then(() => {
+  console.log("Recipe was updated successfully.")
+})
+
+//iteration 5:
+.then(() => {
+  Recipe.deleteOne({title: "Carrot Cake"})
+})
+.then(() => {
+  console.log("Recipe was deleted successfully!")
+})
+
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+
+    // TODO: write the schema
+    title: String,
+    level: String,
+    ingredients: [String],
+    cuisine: String,
+    dishType: String,
+    image: String,
+    duration: Number,
+    creator: String
+  });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
